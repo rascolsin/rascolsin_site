c73e550b6bfd8f4af2ddde5316a05decac57f0d1
Joris Serrand <joris.serrand@rascol.net>
2019-04-05 10:44:58 +0100

    Deployed 0c6215b with MkDocs version: 1.0.4

---
+++ snt/objets_connectes/ressources/Programmes_Python/01-presence_pietons_ELEVE.py
@@ -0,0 +1,12 @@
+# SNT 2019
+# Thème Objets connectés et informatique embarquée
+# Activité Eclairage intelligent
+# Role :
+# ALIMENTER PAR LE  SHIELD GROVE SINON PAS ASSEZ DE COURANT POUR LE PIR
+# V1.0 Date - Nom Prenom Classe
+
+from microbit import *
+pin1.set_pull(pin1.NO_PULL) #Pour fonctionnement PIR
+
+while True:
+    presence_pieton=
